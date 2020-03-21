85df1126ab740937bb7664779e3a431dd801191e
Mike Smith <mosmith@cspire.com>
2020-03-20 20:17:44 -0400

    get user function

--- PSFortiAuthenticator/Public/Get-FAUser.ps1
+++ PSFortiAuthenticator/Public/Get-FAUser.ps1
@@ -35,57 +35,38 @@
     .LINK
     https://docs.fortinet.com/document/fortiauthenticator/6.0.0/rest-api-solution-guide/927310/introduction
 #>
-Function Invoke-FAQuery{
+Function Get-FAUser{
 	[CmdletBinding()]
 	Param([Parameter(Mandatory=$true)]
           [string]
 		  $Server,
-          [Parameter(Mandatory=$true)]
-          [string]
-		  $Resource,
-          [string]
-		  [Parameter(Mandatory=$true)]
-		  [ValidateSet("GET","PUT","PATCH","POST","DELETE")]
-          $Method,
           [Parameter(Mandatory=$false)]
           [string]
 		  $APIUser = $global:FAAPIUser,
           [Parameter(Mandatory=$false)]
           [string]
-		  $APIKey = $global:FAAPIKey,
-          [Parameter(Mandatory=$false)]
-          [switch]
-		  $SkipCertCheck)
+		  $APIKey = $global:FAAPIKey)
 	
 	begin{
-        if($SkipCertCheck){
-        add-type @"
-            using System.Net;
-            using System.Security.Cryptography.X509Certificates;
-            public class TrustAllCertsPolicy : ICertificatePolicy {
-                public bool CheckValidationResult(
-                    ServicePoint srvPoint, X509Certificate certificate,
-                    WebRequest request, int certificateProblem) {
-                    return true;
-                }
-            }
-"@
-        [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-        }
-
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         if(!$APIKey){ Throw "You need to include the secret." }
         if(!$APIUser){ Throw "You need to include a user." }
-        $AuthString = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $APIUser,$APIKey)))
-        $Headers = @{Authorization=("Basic {0}" -f $AuthString)}
-        $BaseUrl = "https://$Server/api/v1/$($Resource)/?format=json&limit=1000"
-        Write-Verbose $BaseUrl
+        $Resource = "localusers"
+        $Method = "GET"
     }
 	process{
 		try{
-            $Results = Invoke-RestMethod -Uri $BaseUrl -Method $Method -Headers $Headers
-            Write-Verbose $Results
-            Write-Output $Results
+            $Params = @{
+                "Server"    = $Server
+                "Resource"  = $Resource
+                "Method"    = $Method
+                "APIUser"   = $APIUser
+                "APIKey"    = $APIKey
+            }
+
+            $Results = Invoke-FAQuery @Params
+            
+            Write-Verbose $Results.meta
+            Write-Output $Results.objects
         }
 		catch{
 			$ErrorMessage = $_.Exception.Message
